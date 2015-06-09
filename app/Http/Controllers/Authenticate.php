6f08b73d88cbc097a3f8475cec6e7eec5153893a
redcode101 <redcode101@bitbucket.org>
2015-06-09 18:35:35 -0500

    new changes

---
+++ app/Http/Controllers/Authenticate.php
@@ -0,0 +1,36 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Auth;
+
+
+class authenticate extends Controller {
+
+	/*
+	|--------------------------------------------------------------------------
+	| Registration & Login Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller handles the registration of new users, as well as the
+	| authentication of existing users. By default, this controller uses
+	| a simple trait to add these behaviors. Why don't you explore it?
+	|
+	*/
+
+	   public function authen()
+    {
+
+    	if (Auth::check())
+		{
+		    echo "IS authenticated";
+		    // do all the database stuff that need to be done like check
+		    // if the user uploaded their resume and stuff
+		}else{
+
+			return redirect()->to('http://login.choicebusinesssolutions.net/start');
+		}
+
+    }
+
+}
