4cb2686ffcf965b314a682bf4a3106a7e353fea0
redcode101 <redcode101@bitbucket.org>
2015-06-25 19:58:42 -0500

    routes2

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,14 +11,24 @@
 |
 */
 
-//Route::get('/', 'WelcomeController@index');
+//Route::get('/', 'HomeController@index');
 Route::get('/', array('as'=>'home','uses'=>'WelcomeController@index'));
 Route::get('/search', array('as'=>'search','uses'=>'Search@search'));
 Route::get('/start', array('as'=>'start','uses'=>'Start@index'));
 Route::get('/apply', array('as'=>'apply','uses'=>'Authenticate@authen'));
-Route::get('/profile', function () {
-    return redirect()->away('localhost/cbs/cbs/public');
-});
+
+//Route::get('/', array('as'=>'fakehome','uses'=>'WelcomeController@newIndex'));
+
+// Route::get('/{id}', function ($id) {
+
+		
+// 			if (empty($id)) {
+// 				return redirect()->Route('home');
+// 			}elseif ($id==2) {
+				
+// 			}
+    
+// });
 //Route::get('/sales', array('as'=>'mod','uses'=>'WelcomeController@index'));
 Route::get('/about', array('as'=>'about','uses'=>'PageController@about'));
 Route::get('/jobs', array('as'=>'job','uses'=>'PageController@jobs'));
@@ -41,31 +51,32 @@ Route::get('/services/operationalProjectManagement', array('as'=>'opm','uses'=>'
 Route::get('/services/fastTrackProjectManagement', array('as'=>'ftpm','uses'=>'PageController@fastTrackProjectManagement'));
 Route::get('/services/customerService', array('as'=>'cus','uses'=>'PageController@customerService'));
 
-//start
- 
-// Route::get('/start/search', array('as'=>'search','uses'=>'Search@search'));
-// Route::get('/start/apply', array('as'=>'apply','uses'=>'Authenticate@authen'));
 
-// Route::get('/start/about', array('as'=>'about2','uses'=>'PageController2@about'));
-// Route::get('/start/jobs', array('as'=>'job','uses'=>'PageController2@jobs'));
-// Route::get('/start/services', array('as'=>'service','uses'=>'PageController2@service'));
+//start
+
+// Route::get('start/search', array('as'=>'search','uses'=>'Search@search'));
+// Route::get('start/apply', array('as'=>'apply','uses'=>'Authenticate@authen'));
+
+// Route::get('start/about', array('as'=>'about','uses'=>'PageController2@about'));
+// Route::get('start/jobs', array('as'=>'job','uses'=>'PageController2@jobs'));
+// Route::get('start/services', array('as'=>'service','uses'=>'PageController2@service'));
 // //Route::get('/job', 'Jobs@index');
 
 
-// Route::get('/start/services/workforce_outsourcing', array('as'=>'outsrc','uses'=>'PageController2@outsourcing'));
-// Route::get('/start/services/training', array('as'=>'trnng','uses'=>'PageController2@training'));
-// Route::get('/start/services/payroll', array('as'=>'pay','uses'=>'PageController2@payRoll'));
-// Route::get('/start/services/hrservices', array('as'=>'hrsrv','uses'=>'PageController2@hrServices'));
-// Route::get('/start/services/recruitment', array('as'=>'recruite','uses'=>'PageController2@recruitment'));
-// Route::get('/start/services/consultation', array('as'=>'consult','uses'=>'PageController2@consultation'));
+// Route::get('start/services/workforce_outsourcing', array('as'=>'outsrc','uses'=>'PageController2@outsourcing'));
+// Route::get('start/services/training', array('as'=>'trnng','uses'=>'PageController2@training'));
+// Route::get('start/services/payroll', array('as'=>'pay','uses'=>'PageController2@payRoll'));
+// Route::get('start/services/hrservices', array('as'=>'hrsrv','uses'=>'PageController2@hrServices'));
+// Route::get('start/services/recruitment', array('as'=>'recruite','uses'=>'PageController2@recruitment'));
+// Route::get('start/services/consultation', array('as'=>'consult','uses'=>'PageController2@consultation'));
 
 
 
-// Route::get('/start/training/projectmanagement', array('as'=>'pgmnt','uses'=>'PageController2@projectManagement'));
-// Route::get('/start/services/onSiteProManagement', array('as'=>'onsite','uses'=>'PageController2@onSiteProManagement'));
-// Route::get('/start/services/operationalProjectManagement', array('as'=>'opm','uses'=>'PageController2@operationalProjectManagement'));
-// Route::get('/start/services/fastTrackProjectManagement', array('as'=>'ftpm','uses'=>'PageController2@fastTrackProjectManagement'));
-// Route::get('/start/services/customerService', array('as'=>'cus','uses'=>'PageController2@customerService'));
+// Route::get('start/training/projectmanagement', array('as'=>'pgmnt','uses'=>'PageController2@projectManagement'));
+// Route::get('start/services/onSiteProManagement', array('as'=>'onsite','uses'=>'PageController2@onSiteProManagement'));
+// Route::get('start/services/operationalProjectManagement', array('as'=>'opm','uses'=>'PageController2@operationalProjectManagement'));
+// Route::get('start/services/fastTrackProjectManagement', array('as'=>'ftpm','uses'=>'PageController2@fastTrackProjectManagement'));
+// Route::get('start/services/customerService', array('as'=>'cus','uses'=>'PageController2@customerService'));
 
 
 
@@ -75,5 +86,3 @@ Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
 ]);
-
-
