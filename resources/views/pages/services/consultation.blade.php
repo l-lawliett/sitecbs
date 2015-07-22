4d89b73ffdc7780118b86c733f295f10185a9892
redcode101 <redcode101@bitbucket.org>
2015-07-22 15:34:56 -0500

    footer an consultation

--- resources/views/pages/services/consultation.blade.php
+++ resources/views/pages/services/consultation.blade.php
@@ -9,26 +9,54 @@ Company:Choices Business Solutions
 @section('content')
 <body class="consult">
 
-		 @include('pages.contentHeader')
-		  <div class="" id="foyer">
+         @include('pages.contentHeader')
+          <div class="" id="foyer">
         </div>
         <div class="container">
             <div class="clearfix" id="main">
                     <div class="col-xs-12 col-md-9 pull-right">
 
-                        <h2>Project Management Consultancy</h2>
+
+                        <h2 class="con_title">Our Leader</h2>
                         <div class="row-divider col-xs-12"></div>
+                        <p>Ms. Janet Smythe Malcolm is an industry leader in providing consulting expertise to our valued clients. 
+                          With multiple projects under her belt, Choice Business Solutions through our CEO Janet Smythe Malcolm is poised to effectively
+                         support your project needs, wherever they may be.</p>
+                        <p>Ms. Janet Smythe Malcolm’s proven service record of Project Management success extends 
+                        worldwide for providing exceptional quality, innovative management and professional integrity in design and management of projects in:</p>
+
+
+
+                         <ul>
+                            <li>Business Process Modeling</li>
+                            <li>Business Solution Analysis</li>
+                            <li>Business requirement review</li>
+                            <li>User Acceptance Testing (UAT)</li>
+                            <li>Human Resource Analysis</li>
+                            <li>HR Operational Process Improvement</li>
+                            <li>HR Best Practice processes and procedures</li>
+                            <li>HR Mergers and Integration</li>
+                        </ul>
+
+<button class='myButton' onclick="location.href='{{ URL::route('service') }}'">
+    <img src="{{ asset("assets/css/images/arrow-7.png") }}"> Learn more about our projects</button>
+
+                         <div class="row-divider col-xs-12"></div>
+
+
+                        <h2 class="con_title">Project Management Consultancy</h2>
+                     
 
                         <p>Our proven track record of successful project management speaks to the high caliber of knowledge and expertise within the CBS team. Each project, like each client, brings its own uniqueness and needs to the fore. As such, our project management consultancy services are applied across various industries with an individualistic approach.</p>
-                        <h3>What we do</h3>
+                        <h3 class="con_title">What we do</h3>
                         <p>We establish a Project Culture in organizations, ensuring the efficient execution of projects directly related to meeting organizational strategies. This includes planning and execution of high impact projects ensuring the desired results within the desired time frame, Business process review and Project Manager Outsourcing.</p>
-                        <h3>How we do it</h3>
+                        <h3 class="con_title">How we do it</h3>
                         <p>At CBS, we sit with the client and take a strategic view of the project objectives and ensure that they are aligned with the overall company goals. We map the processes to identify feasibility, then determine what resources are needed and assign them accordingly. Our internal senior consultants are qualified Project Managers with extensive experience in making strategy a reality through the use of Project Management principles and practices.</p>
 
                         <div class="row-divider col-xs-12"></div>
-                        <h2>HR Consultancy</h2>
+                        <h2 class="con_title">HR Consultancy</h2>
                         <p>People are the most important assets within any organization; their roles and functions along with the equipment and technologies they use. At CBS where it's all about people, we ensure that the best practices and processes are being utilized by our clients.</p>
-                        <h3>What we Do</h3>
+                        <h3 class="con_title">What we Do</h3>
                         <p>We provide consultancy in:</p>
                         <ul>
                             <li>Management of the Human Resources Department to ensure that employers and employees maximize the contribution of human capital to the organization</li>
@@ -38,12 +66,12 @@ Company:Choices Business Solutions
                             <li>Information Systems selection and implementation for HR division</li>
                             <li>HR restructuring</li>
                         </ul>
-                        <h3>How we do it</h3>
+                        <h3 class="con_title">How we do it</h3>
                         <p>We consult with the clients, make an assessment of the needs and propose and implement best solutions. &nbsp;We help to transform your HR Function into a strategic business unit.</p>
 
                         <div class="row-divider col-xs-12"></div>
 
-                        <h2>Business Process Consulting</h2>
+                        <h2 class="con_title">Business Process Consulting</h2>
                         <p>As business analysts, we identify and define the processes that will increase efficiency and maximize the value delivered by an organization to its stakeholders.</p>
                         <p>We do:</p>
                         <ul>
@@ -60,7 +88,7 @@ Company:Choices Business Solutions
                 </div> <!-- end: main -->
                 </div> <!-- end: container -->
 
-		 @include('pages.contentFooter')
+         @include('pages.contentFooter')
                 
 
 
