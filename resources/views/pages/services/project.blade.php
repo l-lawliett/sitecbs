1c0fc5e14c29fd674cbae35265a325b16cd6dc31
redcode101 <redcode101@bitbucket.org>
2015-07-25 07:29:50 -0500

    projcts add

--- resources/views/pages/services/project.blade.php
+++ resources/views/pages/services/project.blade.php
@@ -52,7 +52,7 @@ Company:Choices Business Solutions
                         
             <!-- if current page is single or page show content, else show only excerpt. -->
                             <div class="post_content">
-                <div id="pl-1866"><div class="panel-grid" id="pg-1866-0" ><div class="panel-grid-cell" id="pgc-1866-0-0" ><div class="panel widget widget_black-studio-tinymce panel-first-child panel-last-child" id="panel-1866-0-0-0"><div class="textwidget"><p><a href="/wp-content/uploads/2015/01/arrow-1.png"><img class="alignnone size-full wp-image-1869" src="/wp-content/uploads/2015/01/arrow-1.png" alt="arrow 1" width="148" height="204" /></a></p>
+                <div id="pl-1866"><div class="panel-grid" id="pg-1866-0" ><div class="panel-grid-cell" id="pgc-1866-0-0" ><div class="panel widget widget_black-studio-tinymce panel-first-child panel-last-child" id="panel-1866-0-0-0"><div class="textwidget"><p><a href="assets/wp-content/uploads/2015/01/arrow-1.png"><img class="alignnone size-full wp-image-1869" src="assets/wp-content/uploads/2015/01/arrow-1.png" alt="arrow 1" width="148" height="204" /></a></p>
 </div></div></div><div class="panel-grid-cell" id="pgc-1866-0-1" ><div class="panel widget widget_black-studio-tinymce panel-first-child panel-last-child" id="panel-1866-0-1-0"><div class="textwidget"><p><strong>VICTORIA MUTUAL BUILDING SOCIETY</strong><strong> (</strong><strong>VMBS)</strong><strong>: </strong></p>
 <p><strong> </strong>We successfully planned and implemented various transformational program at Victoria Mutual Building Society.</p>
 </div></div></div></div><div class="panel-grid" id="pg-1866-1" ><div class="panel-grid-cell" id="pgc-1866-1-0" ><div class="panel widget widget_black-studio-tinymce panel-first-child panel-last-child" id="panel-1866-1-0-0"><div class="textwidget"><p><strong>MAYBERRY INVESTMENTS LIMITED: </strong></p>
@@ -68,8 +68,8 @@ Company:Choices Business Solutions
 </li>
 </ol>
 <p> </p>
-</div></div></div><div class="panel-grid-cell" id="pgc-1866-1-1" ><div class="panel widget widget_black-studio-tinymce panel-first-child panel-last-child" id="panel-1866-1-1-0"><div class="textwidget"><p><a href="/wp-content/uploads/2015/01/arrow-6.png"><img class="alignnone size-full wp-image-1870" src="/wp-content/uploads/2015/01/arrow-6.png" alt="arrow 6" width="148" height="203" /></a></p>
-</div></div></div></div><div class="panel-grid" id="pg-1866-2" ><div class="panel-grid-cell" id="pgc-1866-2-0" ><div class="panel widget widget_black-studio-tinymce panel-first-child panel-last-child" id="panel-1866-2-0-0"><div class="textwidget"><p><a href="/wp-content/uploads/2015/01/arrow-2.png"><img class="alignnone size-full wp-image-1868" src="/wp-content/uploads/2015/01/arrow-2.png" alt="arrow 2" width="149" height="203" /></a></p>
+</div></div></div><div class="panel-grid-cell" id="pgc-1866-1-1" ><div class="panel widget widget_black-studio-tinymce panel-first-child panel-last-child" id="panel-1866-1-1-0"><div class="textwidget"><p><a href="assets/wp-content/uploads/2015/01/arrow-6.png"><img class="alignnone size-full wp-image-1870" src="assets/wp-content/uploads/2015/01/arrow-6.png" alt="arrow 6" width="148" height="203" /></a></p>
+</div></div></div></div><div class="panel-grid" id="pg-1866-2" ><div class="panel-grid-cell" id="pgc-1866-2-0" ><div class="panel widget widget_black-studio-tinymce panel-first-child panel-last-child" id="panel-1866-2-0-0"><div class="textwidget"><p><a href="assets/wp-content/uploads/2015/01/arrow-2.png"><img class="alignnone size-full wp-image-1868" src="assets/wp-content/uploads/2015/01/arrow-2.png" alt="arrow 2" width="149" height="203" /></a></p>
 </div></div></div><div class="panel-grid-cell" id="pgc-1866-2-1" ><div class="panel widget widget_black-studio-tinymce panel-first-child panel-last-child" id="panel-1866-2-1-0"><div class="textwidget"><p><strong>VICTORIA MUTUAL WEALTH:</strong></p>
 <p> We successfully initiated, planned and Implemented Mimics Investment software within Victoria Mutual Wealth. We managed all aspects of the project especially the:</p>
 <ol>
@@ -86,8 +86,8 @@ Company:Choices Business Solutions
 <li>User training</li>
 <li>Documentation and implementation of the related Processes and Procedures.</li>
 </ol>
-</div></div></div><div class="panel-grid-cell" id="pgc-1866-3-1" ><div class="panel widget widget_black-studio-tinymce panel-first-child panel-last-child" id="panel-1866-3-1-0"><div class="textwidget"><p><a href="/wp-content/uploads/2015/01/arrow-4.png"><img class="alignnone size-full wp-image-1872" src="/wp-content/uploads/2015/01/arrow-4.png" alt="arrow 4" width="148" height="204" /></a></p>
-</div></div></div></div><div class="panel-grid" id="pg-1866-4" ><div class="panel-grid-cell" id="pgc-1866-4-0" ><div class="panel widget widget_black-studio-tinymce panel-first-child panel-last-child" id="panel-1866-4-0-0"><div class="textwidget"><p><a href="/wp-content/uploads/2015/01/arrow-3.png"><img class="alignnone size-full wp-image-1873" src="/wp-content/uploads/2015/01/arrow-3.png" alt="arrow 3" width="148" height="204" /></a></p>
+</div></div></div><div class="panel-grid-cell" id="pgc-1866-3-1" ><div class="panel widget widget_black-studio-tinymce panel-first-child panel-last-child" id="panel-1866-3-1-0"><div class="textwidget"><p><a href="assets/wp-content/uploads/2015/01/arrow-4.png"><img class="alignnone size-full wp-image-1872" src="assets/wp-content/uploads/2015/01/arrow-4.png" alt="arrow 4" width="148" height="204" /></a></p>
+</div></div></div></div><div class="panel-grid" id="pg-1866-4" ><div class="panel-grid-cell" id="pgc-1866-4-0" ><div class="panel widget widget_black-studio-tinymce panel-first-child panel-last-child" id="panel-1866-4-0-0"><div class="textwidget"><p><a href="assets/wp-content/uploads/2015/01/arrow-3.png"><img class="alignnone size-full wp-image-1873" src="assets/wp-content/uploads/2015/01/arrow-3.png" alt="arrow 3" width="148" height="204" /></a></p>
 </div></div></div><div class="panel-grid-cell" id="pgc-1866-4-1" ><div class="panel widget widget_black-studio-tinymce panel-first-child panel-last-child" id="panel-1866-4-1-0"><div class="textwidget"><p><strong>BRITISH CARIBBEAN INSURANCE COMPANY (BCIC) AND VICTORIA MUTUAL INSURANCE COMPANY (VMIC)</strong><strong>:</strong></p>
 <p> We successfully planned and executed the Integration and merger of the two insurance companies; which require the full project life cycle from detailed planning to closure. The process included a review of the operations of both companies to determine the scope for rationalisation and improvements to be made in operational effectiveness and efficiency.</p>
 </div></div></div></div></div>                                </div>
@@ -124,25 +124,25 @@ Company:Choices Business Solutions
 var _wpcf7 = {"loaderUrl":"http:\/\/www.choicebusinesssolutions.net\/wp-content\/plugins\/contact-form-7\/images\/ajax-loader.gif","sending":"Sending ...","cached":"1"};
 /* ]]> */
 </script>
-<script type='text/javascript' src='/wp-content/plugins/contact-form-7/includes/js/scripts2f54.js?ver=4.1'></script>
-<script type='text/javascript' src='../wp-includes/js/jquery/ui/core.min4a80.js?ver=1.11.2'></script>
-<script type='text/javascript' src='../wp-includes/js/jquery/ui/widget.min4a80.js?ver=1.11.2'></script>
-<script type='text/javascript' src='../wp-includes/js/jquery/ui/accordion.min4a80.js?ver=1.11.2'></script>
-<script type='text/javascript' src='../wp-includes/js/jquery/ui/tabs.min4a80.js?ver=1.11.2'></script>
-<script type='text/javascript' src='/wp-content/plugins/squelch-tabs-and-accordions-shortcodes/js/squelch-tabs-and-accordionsaf8f.js?ver=0.3.4'></script>
-<script type='text/javascript' src='/wp-content/themes/sarraty/framework/bootstrap/js/bootstrap.min3d81.js?ver=4.1.6'></script>
-<script type='text/javascript' src='/wp-content/themes/sarraty/js/waypoints.min3d81.js?ver=4.1.6'></script>
-<script type='text/javascript' src='/wp-content/themes/sarraty/js/jquery.appear3d81.js?ver=4.1.6'></script>
-<script type='text/javascript' src='/wp-content/themes/sarraty/js/jquery.parallax-1.1.33d81.js?ver=4.1.6'></script>
-<script type='text/javascript' src='/wp-content/themes/sarraty/js/jquery.fitvids3d81.js?ver=4.1.6'></script>
-<script type='text/javascript' src='/wp-content/themes/sarraty/js/prettyphoto/js/jquery.prettyPhoto3d81.js?ver=4.1.6'></script>
-<script type='text/javascript' src='/wp-content/themes/sarraty/js/flexslider/jquery.flexslider-min3d81.js?ver=4.1.6'></script>
-<script type='text/javascript' src='/wp-content/themes/sarraty/js/owl-carousel/owl.carousel.min3d81.js?ver=4.1.6'></script>
-<script type='text/javascript' src='/wp-content/themes/sarraty/js/elastic/js/jquery.eislideshow3d81.js?ver=4.1.6'></script>
-<script type='text/javascript' src='/wp-content/themes/sarraty/js/elastic/js/jquery.easing.1.33d81.js?ver=4.1.6'></script>
-<script type='text/javascript' src='/wp-content/themes/sarraty/js/masonry3d81.js?ver=4.1.6'></script>
-<script type='text/javascript' src='/wp-content/themes/sarraty/js/isotope/jquery.isotope.min3d81.js?ver=4.1.6'></script>
-<script type='text/javascript' src='/wp-content/themes/sarraty/js/asalah3d81.js?ver=4.1.6'></script>
+<script type='text/javascript' src='assets/wp-content/plugins/contact-form-7/includes/js/scripts2f54.js?ver=4.1'></script>
+<script type='text/javascript' src='assets/wp-includes/js/jquery/ui/core.min4a80.js?ver=1.11.2'></script>
+<script type='text/javascript' src='assets/wp-includes/js/jquery/ui/widget.min4a80.js?ver=1.11.2'></script>
+<script type='text/javascript' src='assets/wp-includes/js/jquery/ui/accordion.min4a80.js?ver=1.11.2'></script>
+<script type='text/javascript' src='asseyts/wp-includes/js/jquery/ui/tabs.min4a80.js?ver=1.11.2'></script>
+<script type='text/javascript' src='assets/wp-content/plugins/squelch-tabs-and-accordions-shortcodes/js/squelch-tabs-and-accordionsaf8f.js?ver=0.3.4'></script>
+<script type='text/javascript' src='assets/wp-content/themes/sarraty/framework/bootstrap/js/bootstrap.min3d81.js?ver=4.1.6'></script>
+<script type='text/javascript' src='assets/wp-content/themes/sarraty/js/waypoints.min3d81.js?ver=4.1.6'></script>
+<script type='text/javascript' src='assets/wp-content/themes/sarraty/js/jquery.appear3d81.js?ver=4.1.6'></script>
+<script type='text/javascript' src='assets/wp-content/themes/sarraty/js/jquery.parallax-1.1.33d81.js?ver=4.1.6'></script>
+<script type='text/javascript' src='assets/wp-content/themes/sarraty/js/jquery.fitvids3d81.js?ver=4.1.6'></script>
+<script type='text/javascript' src='assets/wp-content/themes/sarraty/js/prettyphoto/js/jquery.prettyPhoto3d81.js?ver=4.1.6'></script>
+<script type='text/javascript' src='assets/wp-content/themes/sarraty/js/flexslider/jquery.flexslider-min3d81.js?ver=4.1.6'></script>
+<script type='text/javascript' src='assets/wp-content/themes/sarraty/js/owl-carousel/owl.carousel.min3d81.js?ver=4.1.6'></script>
+<script type='text/javascript' src='assets/wp-content/themes/sarraty/js/elastic/js/jquery.eislideshow3d81.js?ver=4.1.6'></script>
+<script type='text/javascript' src='assets/wp-content/themes/sarraty/js/elastic/js/jquery.easing.1.33d81.js?ver=4.1.6'></script>
+<script type='text/javascript' src='assets/wp-content/themes/sarraty/js/masonry3d81.js?ver=4.1.6'></script>
+<script type='text/javascript' src='assets/wp-content/themes/sarraty/js/isotope/jquery.isotope.min3d81.js?ver=4.1.6'></script>
+<script type='text/javascript' src='assets/wp-content/themes/sarraty/js/asalah3d81.js?ver=4.1.6'></script>
 
             </div> <!-- end: main -->
         </div> <!-- end: container -->
